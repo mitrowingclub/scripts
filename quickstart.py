9dccacc02325396b45551abaeb99e348a7d2a2b2
Oscar Moll <orm@csail.mit.edu>
2018-09-05 11:29:27 -0400

    creds

--- quickstart.py
+++ quickstart.py
@@ -104,7 +104,7 @@ def main():
     store = file.Storage('token.json')
     creds = store.get()
     if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('credentials_2.json', SCOPES)
+        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
         creds = tools.run_flow(flow, store)
     service = build('sheets', 'v4', http=creds.authorize(Http()))
 
